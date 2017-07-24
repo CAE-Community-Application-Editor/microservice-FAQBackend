654905ab06a2f7ba83d1ce1f99223cdaa0f74aa9
CAEUser <CAEAddress@gmail.com>
2017-07-24 12:04:43 +0200

    Code regeneration/Model synchronization

--- db/FAQBackend_create_tables.sql
+++ db/FAQBackend_create_tables.sql
@@ -9,7 +9,7 @@
 CREATE TABLE caeschema.faq (
   category VARCHAR(255) ,
   answer VARCHAR(255) ,
-  id ID ,
+  id INT NOT NULL AUTO_INCREMENT ,
   question VARCHAR(255) ,
 CONSTRAINT id_PK PRIMARY KEY (id)
  
